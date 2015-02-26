b9ba53b4268ae2242379cb11f429f32081850285
Marco Schlumpp <marco.schlumpp@gmail.com>
2015-02-26 19:00:46 +0100

    Reorganized imports.

--- src/main/scala/de/theunknownxy/jgui/Widget.scala
+++ src/main/scala/de/theunknownxy/jgui/Widget.scala
@@ -1,9 +1,8 @@
 package de.theunknownxy.jgui
 
-import de.theunknownxy.jgui.event.MouseButton
-import MouseButton.MouseButton
-import de.theunknownxy.jgui.base.{Rectangle, Point}
+import de.theunknownxy.jgui.base.{Point, Rectangle}
 import de.theunknownxy.jgui.container.Container
+import de.theunknownxy.jgui.event.MouseButton.MouseButton
 
 abstract class Widget {
   var area: Rectangle = new Rectangle(0, 0, 0, 0)
