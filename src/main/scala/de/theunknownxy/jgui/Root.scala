069850258d7a7321ec79eb8ef481d34108648d12
Marco Schlumpp <marco.schlumpp@gmail.com>
2015-02-26 18:59:44 +0100

    Added method implementations for Root

--- src/main/scala/de/theunknownxy/jgui/Root.scala
+++ src/main/scala/de/theunknownxy/jgui/Root.scala
@@ -1,14 +1,19 @@
 package de.theunknownxy.jgui
 
+import de.theunknownxy.jgui.base.Point
 import de.theunknownxy.jgui.event.MouseButton
 
 import scala.collection.mutable
 
 class Root {
   var named_widgets: mutable.HashMap[String, Widget] = new mutable.HashMap()
-
+  var root_widget: Option[Widget] = None
 
   def injectMouseClick(x: Int, y: Int, button: MouseButton.MouseButton): Unit = {
+    root_widget map (_ mouseClick(new Point(x, y), button))
+  }
 
+  def draw(): Unit = {
+    root_widget map (_ draw())
   }
 }
