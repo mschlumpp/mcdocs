59ac8d23f9adaf1609af0ae208e2d46de11e829c
Marco Schlumpp <marco.schlumpp@gmail.com>
2015-02-27 17:21:50 +0100

    Extend the SingleContainer class for the Root

--- src/main/scala/de/theunknownxy/jgui/Root.scala
+++ src/main/scala/de/theunknownxy/jgui/Root.scala
@@ -1,19 +1,13 @@
 package de.theunknownxy.jgui
 
-import de.theunknownxy.jgui.base.Point
-import de.theunknownxy.jgui.event.MouseButton
+import de.theunknownxy.jgui.container.SingleContainer
 
 import scala.collection.mutable
 
-class Root {
+class Root extends SingleContainer {
   var named_widgets: mutable.HashMap[String, Widget] = new mutable.HashMap()
-  var root_widget: Option[Widget] = None
 
-  def injectMouseClick(x: Int, y: Int, button: MouseButton.MouseButton): Unit = {
-    root_widget map (_ mouseClick(new Point(x, y), button))
-  }
-
-  def draw(): Unit = {
-    root_widget map (_ draw())
+  override protected def recalculateChildren(): Unit = {
+    child map (_.area = this.area)
   }
 }
