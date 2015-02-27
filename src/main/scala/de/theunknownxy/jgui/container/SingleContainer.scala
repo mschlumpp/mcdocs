43a8cf09afa21fabff35ee1f8b4c8a3f9fafb4b9
Marco Schlumpp <marco.schlumpp@gmail.com>
2015-02-27 17:22:19 +0100

    Add default implementations for SingleContainer

--- src/main/scala/de/theunknownxy/jgui/container/SingleContainer.scala
+++ src/main/scala/de/theunknownxy/jgui/container/SingleContainer.scala
@@ -1,11 +1,21 @@
 package de.theunknownxy.jgui.container
 
 import de.theunknownxy.jgui.Widget
+import de.theunknownxy.jgui.base.Point
+import de.theunknownxy.jgui.event.MouseButton.MouseButton
 
 abstract class SingleContainer extends Container {
   var child: Option[Widget] = None
 
   def setChild(widget: Widget): Unit = {
     child = Some(widget)
+  }
+
+  override def mouseClick(pos: Point, button: MouseButton): Option[Widget] = {
+    child filter(_.area contains pos) flatMap(_.mouseClick(pos, button))
+  }
+
+  override def draw(): Unit = {
+    child map (_ draw())
   }
 }
