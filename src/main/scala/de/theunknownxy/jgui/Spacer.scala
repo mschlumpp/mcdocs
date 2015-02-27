fdf21c4a0245abfcf415a26452c45837a53d1985
Marco Schlumpp <marco.schlumpp@gmail.com>
2015-02-27 17:22:37 +0100

    Add a simple Spacer widget

---
+++ src/main/scala/de/theunknownxy/jgui/Spacer.scala
@@ -0,0 +1,9 @@
+package de.theunknownxy.jgui
+
+/** A widget which does nothing
+  *
+  * This widget can be useful for filling space in containers
+  */
+class Spacer extends Widget {
+  override def draw() {}
+}
