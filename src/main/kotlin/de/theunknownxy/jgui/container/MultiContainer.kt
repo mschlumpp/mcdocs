88ae62d2bf17f9acd64fe973920fe80023b09dae
Marco Schlumpp <marco.schlumpp@gmail.com>
2015-02-28 17:29:07 +0100

    Ported VerticalBox to Kotlin

--- src/main/kotlin/de/theunknownxy/jgui/container/MultiContainer.kt
+++ src/main/kotlin/de/theunknownxy/jgui/container/MultiContainer.kt
@@ -4,17 +4,19 @@ import de.theunknownxy.jgui.base.Point
 import de.theunknownxy.jgui.event.MouseButton
 import de.theunknownxy.jgui.base.Widget
 import java.util.ArrayList
+import de.theunknownxy.jgui.layout.Constraint
+import java.util.HashMap
 
 public abstract class MultiContainer : Container() {
-    public var children: MutableList<Widget> = ArrayList()
+    public var children: MutableMap<Widget, Constraint> = HashMap()
 
     override fun draw() {
-        for (w in children) {
+        for (w in children.keySet()) {
             w.draw()
         }
     }
 
     override fun mouseClick(pos: Point, button: MouseButton): Widget? {
-        return children.firstOrNull { w -> w.area.contains(pos) }?.mouseClick(pos, button)
+        return children.keySet().firstOrNull { w -> w.area.contains(pos) }?.mouseClick(pos, button)
     }
 }
--- src/main/kotlin/de/theunknownxy/jgui/container/VerticalBox.kt
+++ src/main/kotlin/de/theunknownxy/jgui/container/VerticalBox.kt
@@ -1,41 +1,40 @@
 package de.theunknownxy.jgui.container
 
-public class VerticalBox {
+import de.theunknownxy.jgui.layout.ExpandingPolicy
+import de.theunknownxy.jgui.layout.FixedPolicy
+
+public class VerticalBox : MultiContainer() {
+    override fun recalculateChildren() {
+        // Step 1: Remove space used by Fixed and collect the total importance of Expanding elements
+        var available_space = this.area.height
+        var expanding_sum = 0f
+        for ((widget, constraint) in children) {
+            val policy = constraint.vertical
+
+            when (policy) {
+                is ExpandingPolicy -> expanding_sum += policy.importance
+                is FixedPolicy -> available_space -= policy.value
+            }
+        }
+
+        // Step 2: Partition remaining space to the expanding widgets
+        for ((widget, constraint) in children) {
+            val policy = constraint.vertical
+            if (policy is ExpandingPolicy) {
+                widget.area.height = policy.importance / expanding_sum * available_space
+            }
+        }
+
+        // Step 3: Adjust widget positions
+        var lasty = area.y
+        for (widget in children.keySet()) {
+            // Set common properties
+            widget.area.x = this.area.x
+            widget.area.width = this.area.width
+
+            // Set calculated properties
+            widget.area.y = lasty
+            lasty += widget.area.height
+        }
+    }
 }
-/* Old Scala code
-import de.theunknownxy.jgui.layout.{Expanding, Fixed}
-class VerticalBoxSc extends MultiContainer {
-  override protected def recalculateChildren(): Unit = {
-
-    // Remove space used by Fixed and collect the total importance of Expanding elements
-    var available_space = this.area.height
-    var expanding_sum = 0.0f
-    for((widget, constraint) <- children) {
-      val policy = constraint.verticalPolicy
-
-      // The scala 'match' seems to be broken
-      if(policy.isInstanceOf[Expanding]) expanding_sum += policy.asInstanceOf[Expanding].importance
-      else if(policy.isInstanceOf[Fixed]) available_space -= policy.asInstanceOf[Fixed].value
-    }
-
-    // Partition remaining space to the expanding widgets
-    for((widget, constraint) <- children) {
-      constraint.verticalPolicy match {
-        case e: Expanding => widget.area.height = e.importance/expanding_sum * available_space
-      }
-    }
-
-    // Adjust positions
-    var lasty = 0.0f
-    for((widget, _) <- children) {
-      // Set common properties
-      widget.area.x = this.area.x
-      widget.area.width = this.area.width
-
-      // Set calculated properties
-      widget.area.y = lasty
-      lasty += widget.area.height
-    }
-  }
-}
-*/
