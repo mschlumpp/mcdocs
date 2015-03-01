231607acb9a1d8f20f9d424e3faca950a7c4fc2c
Marco Schlumpp <marco.schlumpp@gmail.com>
2015-03-01 17:11:16 +0100

    Store the selected widget in Root.

--- src/main/kotlin/de/theunknownxy/jgui/base/Root.kt
+++ src/main/kotlin/de/theunknownxy/jgui/base/Root.kt
@@ -2,9 +2,16 @@ package de.theunknownxy.jgui.base
 
 import de.theunknownxy.jgui.container.SingleContainer
 import java.util.HashMap
+import de.theunknownxy.jgui.event.MouseButton
 
 public class Root : SingleContainer() {
     public var named_widgets: HashMap<String, Widget> = HashMap()
+    public var selected_widget: Widget? = null
+
+    override fun mouseClick(pos: Point, button: MouseButton): Widget? {
+        selected_widget = super.mouseClick(pos, button)
+        return selected_widget
+    }
 
     override fun recalculateChildren() {
         child?.area = area
