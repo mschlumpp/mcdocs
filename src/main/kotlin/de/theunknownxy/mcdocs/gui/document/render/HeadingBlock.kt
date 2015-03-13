1c454c170dc7e5ec3fffb5e4b930d0ea5f19eee0
Marco Schlumpp <marco.schlumpp@gmail.com>
2015-03-13 20:05:40 +0100

    Scale heading depending on level.

--- src/main/kotlin/de/theunknownxy/mcdocs/gui/document/render/HeadingBlock.kt
+++ src/main/kotlin/de/theunknownxy/mcdocs/gui/document/render/HeadingBlock.kt
@@ -5,11 +5,15 @@ import de.theunknownxy.mcdocs.docs.HeadingElement
 import net.minecraft.client.Minecraft
 
 public class HeadingBlock(val heading: HeadingElement) : Block() {
+    private val base_height = 9f
+    private fun font_height(): Float = heading_scale() * base_height
+    private fun heading_scale(): Float = -0.15f * heading.level + 1.5f
+
     override var height: Float
         private set(v: Float) {
         }
         get() {
-            return 14f
+            return font_height()
         }
 
     override fun draw(x: Float, y: Float, z: Float) {
@@ -17,7 +21,7 @@ public class HeadingBlock(val heading: HeadingElement) : Block() {
         GL11.glTranslatef(x, y, z)
 
         // TODO: Scale heading depending on level
-        GL11.glScalef(1.3f, 1.3f, 1f)
+        GL11.glScalef(heading_scale(), heading_scale(), 1f)
         val mc = Minecraft.getMinecraft().fontRenderer
         mc.drawString("§n" + heading.text, 0, 0, 0xFFFFFF)
 
