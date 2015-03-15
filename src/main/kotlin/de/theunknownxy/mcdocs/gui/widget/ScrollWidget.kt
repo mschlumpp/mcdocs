2db2c70b9028705c32bd5da158428ba25c6e524b
Marco Schlumpp <marco.schlumpp@gmail.com>
2015-03-15 13:12:11 +0100

    ScrollWidget: Added some documentation for Range.

--- src/main/kotlin/de/theunknownxy/mcdocs/gui/widget/ScrollWidget.kt
+++ src/main/kotlin/de/theunknownxy/mcdocs/gui/widget/ScrollWidget.kt
@@ -20,18 +20,27 @@ public abstract class ScrollWidget(root: Root?) : Widget(root) {
     }
 
     private data class Range(var start: Float, var stop: Float) {
+        /**
+         * Set the stop position and move the start position accordingly
+         */
         public fun moveStop(pos: Float) {
             val d = pos - stop
             stop = pos
             start += d
         }
 
+        /**
+         * Set the start position and move the stop position accordingly
+         */
         public fun moveStart(pos: Float) {
             val d = pos - start
             start = pos
             stop += d
         }
 
+        /**
+         * Return the distance between the start and the end point
+         */
         public fun distance(): Float = stop - start
     }
 
