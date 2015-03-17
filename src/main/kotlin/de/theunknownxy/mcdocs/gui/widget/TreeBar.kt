adf248ff2f0be1bfb336ff21c4adc810e077ebae
Marco Schlumpp <marco.schlumpp@gmail.com>
2015-03-17 17:13:43 +0100

    TreeBar: Reorganize code to prepare it for ScrollWidget.

--- src/main/kotlin/de/theunknownxy/mcdocs/gui/widget/TreeBar.kt
+++ src/main/kotlin/de/theunknownxy/mcdocs/gui/widget/TreeBar.kt
@@ -10,6 +10,7 @@ import de.theunknownxy.mcdocs.docs.DocumentationNodeRef
 import net.minecraft.client.audio.PositionedSoundRecord
 import net.minecraft.util.ResourceLocation
 import net.minecraft.client.Minecraft
+import java.util.ArrayList
 
 public class TreeBar(root: Root?) : Widget(root) {
     class object {
@@ -17,59 +18,95 @@ public class TreeBar(root: Root?) : Widget(root) {
         val COLOR_ACTIVE = 0x99FF99
         val COLOR_TOPLEVEL = 0xDDDDDD
         val COLOR_SECONDLEVEL = 0xAAAAAA
+        val PADDING_TOP = 4
     }
 
+    private data class Entry(val level: Int, val text: String, val ref: DocumentationNodeRef)
+
+    var dirty = true
     var backend: DocumentationBackend? = null
-    var hovered_item: DocumentationNodeRef? = null
+    var entries: MutableList<Entry> = ArrayList()
+
+    /**
+     * Append 'num' spaces to a StringBuilder
+     */
+    private fun add_space(sb: StringBuilder, num: Int) = num * { sb.append(' ') }
+
+    /**
+     * Add the childs of 'node' to the entries.
+     */
+    private fun add_children(node: DocumentationNodeRef, max_level: Int, level: Int = 0) {
+        val backend = backend
+        if (backend != null && level < max_level) {
+            val children = backend.getChildren(node)
+            for (child in children) {
+                val numchilds = backend.getChildren(child).size()
+
+                val sb = StringBuilder()
+                if (numchilds > 0) {
+                    // Make nodes with children italic
+                    sb.append("§o")
+                }
+                add_space(sb, level)
+                sb.append(backend.getTitle(child))
+
+                entries.add(Entry(level, sb.toString(), child))
+                add_children(child, max_level, level + 1)
+            }
+        }
+    }
+
+    /**
+     * Recreate the entries in the TreeBar after something changed.
+     */
+    private fun rebuild() {
+        val backend = backend
+        if (backend != null) {
+            entries.clear()
+            if (backend.current_path != backend.root) {
+                // Draw the up entry
+                val title = backend.getTitle(backend.current_path)
+                entries.add(Entry(0, ".. §o[$title]", backend.current_path.parent()))
+            }
+
+            add_children(backend.current_path, 2)
+            dirty = false
+        }
+    }
 
     override fun draw() {
         val fontrenderer = root?.gui?.mc?.fontRenderer
         val backend = backend!!
         if (fontrenderer != null) {
-            var dy = 4
+            if (dirty) rebuild()
 
-            fun drawEntry(node: DocumentationNodeRef, text: String, color: Int) {
-                var ecol = color
+            var dy = PADDING_TOP
+            for ((level, text, ref) in entries) {
+                var ecol = 0x000000
+                // Color depending on depth
+                if (level == 0) {
+                    ecol = COLOR_TOPLEVEL
+                } else if (level == 1) {
+                    ecol = COLOR_SECONDLEVEL
+                }
+
+                // Override color if the node is active or highlighted
                 if (Rectangle(this.x, this.y + dy, this.width, fontrenderer.FONT_HEIGHT - 1f).contains(root!!.mouse_pos)) {
-                    // Highlight item on hover and store it
                     ecol = COLOR_HIGHLIGHT
-                    hovered_item = node
-                } else if(node == backend.current_page) {
+                } else if (ref == backend.current_page) {
                     ecol = COLOR_ACTIVE
                 }
                 fontrenderer.drawString(text, this.x.toInt(), (this.y + dy).toInt(), ecol)
                 dy += fontrenderer.FONT_HEIGHT
             }
-
-            if (backend.current_path != backend.root) {
-                // Draw the up entry
-                val title = backend.getTitle(backend.current_path)
-                drawEntry(backend.current_path.parent(), ".. §o[$title]", COLOR_TOPLEVEL)
-            } else {
-                // Or leave some space
-                dy += fontrenderer.FONT_HEIGHT
-            }
-            val children = backend.getChildren(backend.current_path)
-            for (child in children) {
-                val title = backend.getTitle(child)
-                drawEntry(child, title, COLOR_TOPLEVEL)
-                val childchildren = backend.getChildren(child)
-                for (childchild in childchildren) {
-                    val childtitle = backend.getTitle(childchild)
-                    val childchildchildren = backend.getChildren(childchild)
-                    var prefix = "  "
-                    if(childchildchildren.size() > 0) {
-                        prefix = " +"
-                    }
-                    drawEntry(childchild, prefix + childtitle, COLOR_SECONDLEVEL)
-                }
-            }
         }
     }
 
     override fun onMouseClick(pos: Point, button: MouseButton): Widget? {
-        if(hovered_item != null && button == MouseButton.LEFT) {
-            backend?.navigate(hovered_item!!)
+        val index = ((pos.y - y - PADDING_TOP) / Minecraft.getMinecraft().fontRenderer.FONT_HEIGHT.toFloat()).toInt()
+        if (index < entries.size()) {
+            backend?.navigate(entries[index].ref)
+            dirty = true
             Minecraft.getMinecraft().getSoundHandler().playSound(PositionedSoundRecord.func_147674_a(ResourceLocation("gui.button.press"), 1.toFloat()))
         }
         return null
