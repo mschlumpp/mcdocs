c288a532fd515040c72c0070e3b13e7ef9c27ed3
Marco Schlumpp <marco.schlumpp@gmail.com>
2015-03-31 17:21:33 +0200

    Point: Removed old file header.

--- src/main/kotlin/de/theunknownxy/mcdocs/gui/base/Point.kt
+++ src/main/kotlin/de/theunknownxy/mcdocs/gui/base/Point.kt
@@ -1,8 +1,5 @@
 package de.theunknownxy.mcdocs.gui.base
 
-/**
- * Created by marco on 2/28/15.
- */
 public class Point(var x: Float, var y: Float) {
     override fun equals(other: Any?): Boolean {
         if (other != null) {
