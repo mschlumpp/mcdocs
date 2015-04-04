eff9fa5daf9d9d6f489d907b40d933ca64001502
Marco Schlumpp <marco.schlumpp@gmail.com>
2015-04-04 17:53:25 +0200

    ImageSegment: Load images from the filesystem if the path doesn't contain a ":"

--- src/main/kotlin/de/theunknownxy/mcdocs/gui/document/segments/ImageSegment.kt
+++ src/main/kotlin/de/theunknownxy/mcdocs/gui/document/segments/ImageSegment.kt
@@ -4,15 +4,50 @@ import de.theunknownxy.mcdocs.docs.ImageBlock
 import de.theunknownxy.mcdocs.gui.document.Document
 import de.theunknownxy.mcdocs.utils.GuiUtils
 import net.minecraft.client.Minecraft
+import net.minecraft.client.renderer.texture.DynamicTexture
 import net.minecraft.util.ResourceLocation
 import org.lwjgl.opengl.GL11
+import java.io.File
+import java.io.IOException
+import javax.imageio.ImageIO
 
-class ImageSegment(document: Document, val elem: ImageBlock) : Segment(document) {
+class ImageSegment(document: Document, private val elem: ImageBlock) : Segment(document) {
     companion object {
         private val PADDING_BOTTOM = 3
     }
 
-    val resloc = ResourceLocation(elem.src)
+    private var img_width = 0
+    private var img_height = 0
+
+    private var resource_loc: ResourceLocation? = null
+    private var dynamic_tex: DynamicTexture? = null
+    private var lazy_init = false
+
+    private fun lazy_init() {
+        if (!lazy_init) {
+            if (elem.src.contains(":")) {
+                resource_loc = ResourceLocation(elem.src)
+                img_width = elem.width
+                img_height = elem.height
+            } else {
+                //TODO: Store the data dir somewhere else
+                val mcdocs_dir = File(Minecraft.getMinecraft().mcDataDir, "doc")
+                val img_path = File(mcdocs_dir, elem.src)
+                try {
+                    val buftex = ImageIO.read(img_path)
+                    dynamic_tex = DynamicTexture(buftex)
+                    resource_loc = Minecraft.getMinecraft().getTextureManager().getDynamicTextureLocation(elem.src, dynamic_tex)
+                    // Scale u/v to 256x256
+                    img_width = (elem.width.toFloat() / buftex.getWidth() * 256).toInt()
+                    img_height = (elem.height.toFloat() / buftex.getHeight() * 256).toInt()
+                } catch(e: IOException) {
+                    println("Cannot read image from path '$img_path'")
+                }
+            }
+
+            lazy_init = true
+        }
+    }
 
     override val height: Float
         get() {
@@ -21,8 +56,13 @@ class ImageSegment(document: Document, val elem: ImageBlock) : Segment(document)
         }
 
     override fun draw() {
-        Minecraft.getMinecraft().getTextureManager().bindTexture(resloc)
-        GL11.glColor3f(1f, 1f, 1f)
-        GuiUtils.drawTexturedModalRect(x.toInt(), y.toInt(), 0.toDouble(), 0, 0, width.toInt(), height.toInt() - PADDING_BOTTOM, elem.width, elem.height)
+        lazy_init()
+
+        val resloc = resource_loc
+        if (resloc != null) {
+            Minecraft.getMinecraft().getTextureManager().bindTexture(resloc)
+            GL11.glColor3f(1f, 1f, 1f)
+            GuiUtils.drawTexturedModalRect(x.toInt(), y.toInt(), 0.toDouble(), 0, 0, width.toInt(), height.toInt() - PADDING_BOTTOM, img_width, img_height)
+        }
     }
 }
